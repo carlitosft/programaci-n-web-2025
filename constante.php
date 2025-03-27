d99ee11005ffec8e100958c8bf3f985a9fc0df2d
carlitosft <carlostttt2023@gmail.com>
2025-03-27 15:39:36 -0300

    constantes en php y actividad

--- constante.php
+++ constante.php
@@ -7,6 +7,6 @@ const gravedad = 12.1;
  2) el erro indica que no se pueden declarar 2 valores a una constante 
  3) cuando querramos dejar fijo un valor como por ejemplo el de la gravedad,
  luego solo se deberia de usar el nombre de la variable en ves de poner el valor,
-  lo que hace mas simple el trabajo 
+  lo que hace mas simple el trabajo.
  */ 
  ?>
