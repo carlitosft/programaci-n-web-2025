f3a2242b6a75d9a3f29e38fcdf2ccc3084072a36
carlitosft <carlostttt2023@gmail.com>
2025-03-27 15:37:38 -0300

    prueba de constantes y actividad

---
+++ constante.php
@@ -0,0 +1,12 @@
+<?php
+echo "constantes";
+const gravedad = 3.14;
+const gravedad = 12.1;
+ /* 1) el error fue que no hay forma de cambiar el valor de una constante,
+ paraeso se usa el valor de una variable
+ 2) el erro indica que no se pueden declarar 2 valores a una constante 
+ 3) cuando querramos dejar fijo un valor como por ejemplo el de la gravedad,
+ luego solo se deberia de usar el nombre de la variable en ves de poner el valor,
+  lo que hace mas simple el trabajo 
+ */ 
+ ?>
