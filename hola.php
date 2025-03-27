50950a3ac50ee13ddc630cf34b42a58306a78601
carlitosft <carlostttt2023@gmail.com>
2025-03-27 14:41:39 -0300

    variables en php

---
+++ hola.php
@@ -0,0 +1,25 @@
+<?php echo "Hola mundo";
+// HOLA AHO
+// FGDGDG
+/* KDJFKJFKSJFSKLFJSKLFJSKLJFKLSFSJ
+FHSJFHSKSKFHSKFHSKFSHFKSHFK*/ 
+$nombreCompleto = "Juan Perez"; 
+$edad = 25; //esta biem
+$estAtura = 1.75; //deberia usar solo letras minusculas
+$numero = "10"; //si el dato esun numero no lleva comillas
+Apellido = "Fernandez"; //no guarda el dato, no tiene $
+pais_de_origen= "Uruguay"; //no guarda el dato
+$Salario =$2000; //elnombre de la variable deberia ser $salarioPesos y el dato solo 2000
+$mayorEdad = false;
+
+
+
+
+
+
+
+
+
+
+
+?> 
