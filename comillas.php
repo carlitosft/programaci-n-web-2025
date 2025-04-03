b7bc2c534f02ed9cad7b05e4fdac025ea9595808
carlitosft <carlostttt2023@gmail.com>
2025-04-03 13:57:59 -0300

    trabajo con tipos decomillas

---
+++ comillas.php
@@ -0,0 +1,7 @@
+<?php
+$emisora = 'Radio Disney';
+echo 'Me gusta escuchar $emisora';
+echo "Megusta escuchar $emisora";
+/*Comillas simples (') no interpretan variables ni secuencias de escape (salvo las excepciones mencionadas).
+Comillas dobles (") interpretan las variables y las secuencias de escape.*/
+?>
