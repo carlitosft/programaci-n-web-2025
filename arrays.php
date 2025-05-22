fd69462ce4effd839a1b956862105d3869a344ae
carlitosft <carlostttt2023@gmail.com>
2025-05-22 14:43:33 -0300

    pruebas con foreach

---
+++ arrays.php
@@ -0,0 +1,16 @@
+<?php
+$contLab = 0;
+$contNo = 0;
+$diasLab = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+foreach ($diasLab as $dias ) {
+    if ($dias == 'sabado' || $dias == 'domingo'){
+echo $dias . " no laborable" . "<br>";
+$contNo ++;
+}else{
+    echo $dias . " laborable" . "<br>";
+    $contLab ++;
+}
+
+}
+echo "dias laborables:" . $contLab . "<br>" . "dias no laborables:" . $contNo;
+?>
