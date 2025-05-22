a6ff321046d7c1fdc7360c861d6723a928bdb052
carlitosft <carlostttt2023@gmail.com>
2025-05-22 15:35:34 -0300

    parte 1

---
+++ practico.php
@@ -0,0 +1,13 @@
+<?php
+$tripulacion = [
+    'Alex Raider',
+    'Mia Kovalik',
+    'Raj Patel',
+    'Elena Vega',
+    'Jules Bergman'
+];
+$tripulacion[] = 'Sam Cartyer' ;
+$tripulacion[] = 'Leo Wu';
+$tripulacion[] =  'Zara Khan';
+var_dump($tripulacion);
+?>
