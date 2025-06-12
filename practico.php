76d6af93ad1e6169072c8aeb26238284ac8f5414
carlitosft <carlostttt2023@gmail.com>
2025-06-12 15:38:25 -0300

    dale

--- practico.php
+++ practico.php
@@ -9,5 +9,24 @@ $tripulacion = [
 $tripulacion[] = 'Sam Cartyer' ;
 $tripulacion[] = 'Leo Wu';
 $tripulacion[] =  'Zara Khan';
-var_dump($tripulacion);
+
+
+function agregar($nuevoNombre,&$Arreglo) {
+    $Arreglo[]=$nuevoNombre;
+}
+agregar('jose', $tripulacion);
+
+
+function evacuar(&$tripulacion){
+    $ultimoValor=array_pop($tripulacion);
+    echo "El tripulante evacuasdo es: ". $ultimoValor;
+
+
+}
+evacuar(     $tripulacion);
+print_r($tripulacion);
+
+
+
+
 ?>
