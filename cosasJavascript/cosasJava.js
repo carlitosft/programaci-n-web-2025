08c72a9088c338e49f44c345938501ed3328e3e8
carlitosft <carlostttt2023gmail.com>
2025-08-28 13:57:30 -0300

    cosas de java

--- cosasJavascript/cosasJava.js
+++ cosasJavascript/cosasJava.js
@@ -1,5 +1,5 @@
 
-let edad = 17;
+/*let edad = 17;
 
 
 const radio = prompt("cual es el radio del circulo" );
@@ -18,4 +18,18 @@ console.log(calcularArea(radio));
 const nombre = prompt("cual es tu nombre");
 const leGusta = prompt("te gusta javascript?");
 console.log("tu nombre es" + nombre);
-console.log("te gusta javascript?" + leGusta);
+console.log("te gusta javascript?" + leGusta);*/
+
+
+
+const ancho = prompt("cual es el ancho de tu rectangulo?");
+const anchoRectangulo = parseInt(ancho);
+
+const alto = prompt("cual es el alto de tu rectangulo?");
+const altoRectangulo = parseInt(alto);
+
+function calculaRectangulo(ancho, alto){
+let areaRectangulo = alto* ancho;
+return areaRectangulo
+}
+console.log(calculaRectangulo(ancho, alto));
